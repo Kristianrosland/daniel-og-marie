c3b28b8443a86b89cc38a96a542a8f5c07a63fbe
Kristian Rosland <kristian.rosland@gmail.com>
2020-03-07 17:52:39 +0100

    Updates

---
+++ precache-manifest.fd32303f017c21eab2db58b7901cfcf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b23080235055c52df42ee741c31e5a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a5e4843bd12498902bd",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8e5404cd68105225b313",
+    "url": "/static/js/2.846dc8d0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.846dc8d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a5e4843bd12498902bd",
+    "url": "/static/js/main.b490f044.chunk.js"
+  },
+  {
+    "revision": "47eac378be116ac93a34",
+    "url": "/static/js/runtime-main.36931019.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
