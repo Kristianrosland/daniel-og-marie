24e6801d19fe752d090de5f1da7557ff493fb91f
Kristian Rosland <kristian.rosland@gmail.com>
2020-03-08 18:30:20 +0100

    Updates

---
+++ precache-manifest.e3f9ed127e7a0887715ecbd646abcbd1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5301d285a63560a541a0ba27c9f099",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76dfffb7f2b3a6800f43",
+    "url": "/static/css/main.6d6ae42e.chunk.css"
+  },
+  {
+    "revision": "feb88307e3758e850bb0",
+    "url": "/static/js/2.b3b5be97.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.b3b5be97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76dfffb7f2b3a6800f43",
+    "url": "/static/js/main.43112e69.chunk.js"
+  },
+  {
+    "revision": "47eac378be116ac93a34",
+    "url": "/static/js/runtime-main.36931019.js"
+  },
+  {
+    "revision": "17a9732692e300f717d1785e9d2a281f",
+    "url": "/static/media/elv.17a97326.png"
+  },
+  {
+    "revision": "f52cb24d08ff910e0b9df8e6ab8fb111",
+    "url": "/static/media/svolvaer.f52cb24d.png"
+  },
+  {
+    "revision": "837612e7c539c402428b14a2c93e09ff",
+    "url": "/static/media/toastmasters.837612e7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63f17f49d434cc5fecf826c906843f2a.js"
+  "/precache-manifest.e3f9ed127e7a0887715ecbd646abcbd1.js"
 );
 
 self.addEventListener('message', (event) => {
