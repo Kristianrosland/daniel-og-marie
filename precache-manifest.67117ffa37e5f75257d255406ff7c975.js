bb3eeacee435cbcaaafe97cd029049c5095f26c5
Kristian Rosland <kristian.rosland@gmail.com>
2020-03-13 00:21:09 +0100

    Updates

---
+++ precache-manifest.67117ffa37e5f75257d255406ff7c975.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4267fa3540c26e4110d233230e1ad6ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4f4085a51538c80a958",
+    "url": "/static/css/main.87f6a07b.chunk.css"
+  },
+  {
+    "revision": "8d47ddfdb4c11b313f0a",
+    "url": "/static/js/2.0fdbbfae.chunk.js"
+  },
+  {
+    "revision": "d68bd37f2996645b1bb3e9deceae2beb",
+    "url": "/static/js/2.0fdbbfae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f4085a51538c80a958",
+    "url": "/static/js/main.737aa727.chunk.js"
+  },
+  {
+    "revision": "47eac378be116ac93a34",
+    "url": "/static/js/runtime-main.36931019.js"
+  },
+  {
+    "revision": "5fae9f581558bc85bb55fadd7abcdf9a",
+    "url": "/static/media/checkmark.5fae9f58.svg"
+  },
+  {
+    "revision": "e660df7c828a4df6d1521aceaec0011e",
+    "url": "/static/media/cross.e660df7c.svg"
+  },
+  {
+    "revision": "c4408d7469b9fa848dc3834b02b3ed3e",
+    "url": "/static/media/edit.c4408d74.svg"
+  },
+  {
+    "revision": "17a9732692e300f717d1785e9d2a281f",
+    "url": "/static/media/elv.17a97326.png"
+  },
+  {
+    "revision": "75bd23f0fa8203b78f588d6d365182fa",
+    "url": "/static/media/right-chevron.75bd23f0.svg"
+  },
+  {
+    "revision": "6560ebb772864e05292a43b5ab619936",
+    "url": "/static/media/servise.6560ebb7.png"
+  },
+  {
+    "revision": "f52cb24d08ff910e0b9df8e6ab8fb111",
+    "url": "/static/media/svolvaer.f52cb24d.png"
+  },
+  {
+    "revision": "837612e7c539c402428b14a2c93e09ff",
+    "url": "/static/media/toastmasters.837612e7.png"
+  }
+]);
