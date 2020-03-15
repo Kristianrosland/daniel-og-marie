e20921866892427d48c99901a8c7f8ee07f539db
mathilde.warstad <mathildewaerstad@gmail.com>
2020-03-15 16:05:12 +0100

    Hente ut spørsmål og svar fra firebase

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,47 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "./faq.less";
+import sortBy from "lodash.sortby";
+import * as firebase from "firebase/app";
+require("firebase/firestore");
 
 const FAQ = ({ refProp }) => {
+  const [faq, setFaq] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("faq")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          setFaq(prevFaq => [...prevFaq, doc.data()]);
+        });
+      });
+  }, []);
+
+  const alleSporsmalOgSvar = sortBy(faq, "index");
+
   return (
     <div ref={refProp} className={css.container}>
       <h2 className={css.header}>Spørsmål og svar</h2>
       <ul className={css.faqList}>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
-        <li className={css.faq}>
-          <div className={css.questionContainer}>
-            <div className={css.question}>Spørsmål</div>
-          </div>
-          <div className={css.answer}>Svar</div>
-        </li>
+        {alleSporsmalOgSvar.map(sporsmalOgSvar => (
+          <li className={css.faq} key={sporsmalOgSvar.index}>
+            <div className={css.questionContainer}>
+              <div className={css.question}>{sporsmalOgSvar.sporsmal}</div>
+            </div>
+            <div className={css.answer}>{sporsmalOgSvar.svar}</div>
+          </li>
+        ))}
       </ul>
       <p className={css.contactInformation}>
         Hvis du lurer på noe kan du ringe Daniel på 45 28 55 79
