29a26c8156c8c82ac619b245e922f2fe626eb8ee
mathilde.warstad <mathildewaerstad@gmail.com>
2020-03-15 15:21:19 +0100

    Utlisting av spørsmål og svar

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -2,7 +2,51 @@ import React from "react";
 import css from "./faq.less";
 
 const FAQ = ({ refProp }) => {
-  return <div ref={refProp} className={css.container} />;
+  return (
+    <div ref={refProp} className={css.container}>
+      <ul className={css.faqList}>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+        <li className={css.faq}>
+          <div className={css.questionContainer}>
+            <div className={css.question}>Spørsmål</div>
+          </div>
+          <div className={css.answer}>Svar</div>
+        </li>
+      </ul>
+      <p className={css.contactInformation}>
+        Hvis du lurer på noe kan du ringe Daniel på 45 28 55 79
+      </p>
+    </div>
+  );
 };
 
 export default FAQ;
